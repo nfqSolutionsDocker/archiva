ac2487cda14d98aa24692f12d2f3b76d241829fa
usuario <usuario@LAPTOP-OPHQGTON>
2017-01-25 17:05:07 +0100

    Solved error in generate files in volumen

--- archiva.sh
+++ archiva.sh
@@ -9,7 +9,7 @@ if [ ! -f /solutions/app/java/bin/java ]; then
     	"http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}-b14/jdk-${JAVA_VERSION}-linux-x64.tar.gz"
 	tar -xvzf /solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz -C /solutions/app/
 	chmod -R 777 $(ls -d /solutions/app/jdk*/)
-	ln -sf $(ls -d /solutions/app/jdk*/) /solutions/app/java
+	mv $(ls -d /solutions/app/jdk*/) /solutions/app/java
 fi
 
 echo Instalando archiva ...
@@ -17,7 +17,7 @@ if [ ! -f /solutions/app/archiva/bin/archiva ]; then
 	wget -P /solutions/app/ "http://archive.apache.org/dist/archiva/2.2.0/binaries/apache-archiva-2.2.0-bin.tar.gz"
 	tar -xvzf /solutions/app/apache-archiva-${ARCHIVA_VERSION}-bin.tar.gz -C /solutions/app/
 	chmod -R 777 $(ls -d /solutions/app/apache-archiva*/)
-	ln -sf $(ls -d /solutions/app/apache-archiva*/) /solutions/app/archiva
+	mv $(ls -d /solutions/app/apache-archiva*/) /solutions/app/archiva
 	mkdir -p /solutions/app/archiva/repositories
 fi
 
